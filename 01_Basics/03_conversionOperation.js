4737e2b59280471635af08dbf633ec8ed4078207
TarunRana1286 <tarunrana237h@gmail.com>
2025-11-25 17:41:37 +0000

    conversion file

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="33ac"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn=null;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1==true; 0== false;
+// ""==false; "anyvalue"==true
+//null==false; undefined==false;
+
+let stringnumber=33
+
+let snumber=String(stringnumber)
+
+console.log(typeof snumber);
+
+
+
