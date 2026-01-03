2345609c259378768ab90295fe6071ec2d7c91a9
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-03 09:54:43 +0000

    math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,48 @@
+const score=400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const othervalue=1123.6698
+
+// console.log(othervalue.toPrecision(3));
+// console.log(othervalue.toPrecision(2));
+// console.log(othervalue.toPrecision(4));
+
+const hundreds =1000000
+
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ************* MATHS ***************//
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(7.5));
+// console.log(Math.ceil(7.5));
+// console.log(Math.floor(7.5));
+// console.log(Math.min(1,5,6,9,7));
+// console.log(Math.max(1,5,6,9,7));
+
+
+// console.log(Math.random()); // Always Between 0 to 1
+
+// console.log((Math.random()* 10) +1);
+// console.log(Math.floor((Math.random()* 10)) +1);
+
+const min=10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) +min);
+
+
+
+
+
+
+
