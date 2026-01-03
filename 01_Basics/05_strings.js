d80a202d0999e7a19004c05dba7e0f59891840dc
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-03 09:24:34 +0000

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -5,7 +5,7 @@ const repoCount=100
 
 //console.log(`Hellllo My Name is ${name} and My Repocount is ${repoCount}`);
 
-const gameName = new String('Tarun Rana');
+const gameName = new String('Tarun-Rana-1286');
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,8 +13,31 @@ const gameName = new String('Tarun Rana');
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase);
 
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('R'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('R'));
+
+const string=gameName;
+//console.log(string.substring(0, 4));
+
+const anotherstring =gameName
+//console.log(anotherstring.slice (-4,0));
+
+
+const newstring=gameName
+// console.log(newstring);
+// console.log(newstring.trim());
+
+
+const url="https://tarunrana.com/tarun%20Rana"
+console.log(url.replace('%20','-'));
+console.log(url.includes('rana'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
 
 
 
