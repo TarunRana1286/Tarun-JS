9be96c5e1bb79f244055e2d1e03be51ac89355f4
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-08 03:50:21 +0000

    Objects

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,58 @@
+//const whatsapp =new Object();
+
+const whatsapp ={}
+
+whatsapp.id="123abc"
+whatsapp.name = "Rana"
+whatsapp.isLoggedIn=false
+//console.log((whatsapp));
+
+const regularUser = {
+    email : "tarun@test.com",
+    userName : {
+        fullName :{
+            firstName : "Tarun",
+            lastName : "Rana"
+        }
+    }
+}
+
+// console.log(regularUser);
+// console.log(regularUser.userName);
+// console.log(regularUser.userName.fullName);
+// console.log(regularUser.userName.fullName.firstName);
+
+const obj1 = {1: "A", 2: "B"}
+const obj2 = {3: "C", 4: "D"}
+const obj4 = {5: "E", 6: "F"}
+
+//const  obj3 = Object.assign({}, obj1,obj4)
+//const obj3 = {obj1, obj2}
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+//console.log(obj3);
+
+const users =[
+    {
+        id : 1,
+        email : "tarun@gmail.com"
+    },
+    {
+        id : 2,
+        email : "tarun@yahoo.com"
+    },
+    {
+        id : 3,
+        email : "tarun@meta.com"
+    },
+]
+
+//console.log(whatsapp);
+// console.log(Object.keys(whatsapp));
+// console.log(Object.values(whatsapp));
+// console.log(Object.entries(whatsapp));
+
+console.log(whatsapp.hasOwnProperty('isLoggedIn'));
+
+
+
