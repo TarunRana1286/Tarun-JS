7a6b9325184fc9ce6366dd96f3d32db5ecff043f
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-09 18:47:13 +0000

    IIFE

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,23 @@
+// Immedietly Invoked Function Expression
+
+// When you declare function like below u dont need to call function "IIFE" as its in iife effect
+
+// To avoid global scope's pollution as well as Immediate function call
+(function chai (){
+    //Names IIFE
+    console.log(`Tarun Rana`);
+    
+})();
+
+// If you do not use semicolon (;) for iife function it would not know where to stop
+( () =>{
+    console.log(`Rana Tarun`);
+} 
+)();
+
+
+( (lastname) =>{
+    // Parameter or unnamed iife
+    console.log(`Rana Tarun ${lastname}`);
+} 
+)('Parso')
