637f3bee7d92d4ea3771b4865f15cc83b1873a62
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-10 17:33:59 +0000

    Sample Function

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -26,3 +26,16 @@
 // After this JS Execution Concept
 
 // {} Global EC eg. (this)
+
+
+let val1=10
+let val2=20
+
+function addnum(val1,val2){
+
+    let total= val1 + val2
+    return total
+}
+
+let result1 = addnum(val1,val2)
+let result2 = addnum(10,5)
