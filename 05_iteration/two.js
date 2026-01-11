cf714064736237b1d85bc39d11b10ccfe933113f
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-11 18:45:17 +0000

    loops with statements

---
+++ 05_iteration/two.js
@@ -0,0 +1,15 @@
+
+
+// let index =0
+// while (index <= 10) {
+//  console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myarray = ["Thor", "Ironman", "Bucky"]
+let arr = 0
+while (arr  < myarray.length) {
+ //   console.log(`value is ${myarray[arr]}`);
+    arr = arr + 1
+    
+}
