2209a1768660d3f852d466566951d894e7ab071c
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-11 18:21:02 +0000

    Looping

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -9,7 +9,20 @@ for (let index = 0; index < 10; index++) {
     //console.log(element);
 }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i < 10; i++) {
+    //console.log(`Outer Loop Value ${i}`);
+    
+    for (let j = 0; j <= 10 ; j++) {
+        //console.log(`Inner Loop Value ${j} and inner Loop ${i}`);
+       // console.log(i + '*' + j + '=' + i * j);
+    }
+    
+}
+
+let myarray = ["Thor", "Flash", "Batman"]
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+    
     
 }
