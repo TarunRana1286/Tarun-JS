8de97f5cf2dff8e1b162bc468143ee9a1772301a
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-11 18:01:30 +0000

    loopssss

---
+++ 05_iteration/one.js
@@ -0,0 +1,15 @@
+// for
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+
+    if(index == 5){
+      //  console.log("5 i number");
+    }
+    //console.log(element);
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
