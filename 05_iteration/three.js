cae17c99c452d59b593557f41c62a0caae634825
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-12 03:53:57 +0000

    For Of Loops

---
+++ 05_iteration/three.js
@@ -0,0 +1,29 @@
+// for of
+
+//["","",""] Array
+
+//[{},{},{}]  Array Of Objects
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const strarr = ["Tarun","Rana","Ahmedabad"]
+
+for (const name of strarr) {
+    //console.log(name);
+}
+
+const greetings = "Hello Tarun!"
+
+for (const greet of greetings) {
+   // console.log(`Each Char is ${greet}`);
+}
+
+// MAPS
+
+const map = new Map()
+
+map
