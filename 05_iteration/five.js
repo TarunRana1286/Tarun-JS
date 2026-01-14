21c1bc8bc5bc5f92fd8392c6ab73a16597dcd021
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-14 18:40:09 +0000

    for each results

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,3 +1,23 @@
 const coding = ["js","C#","java","python","cpp"]
 
-coding.forEach()
+// coding.forEach( function (val){
+//     console.log(val);
+// })
+
+
+// Using Arrow function
+// coding.forEach( (item) =>{
+//     console.log(item);
+// })
+
+
+// with reference calling
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach( (item,index,arr) =>{
+console.log(item,index,arr);
+})
