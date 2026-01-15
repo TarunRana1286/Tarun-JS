51e965cc7d15cdb2f8e71ee092861dde7b8c139e
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-15 16:01:34 +0000

    foreach with array/objects

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -19,5 +19,32 @@ const coding = ["js","C#","java","python","cpp"]
 // coding.forEach(printMe)
 
 coding.forEach( (item,index,arr) =>{
-console.log(item,index,arr);
+//console.log(item,index,arr);
+})
+
+
+const mycoding = [
+    {
+        LanguageName : "Javascript",
+        LanguageFile : "js"
+    },
+    {
+        LanguageName : "Java",
+        LanguageFile : "java"
+    },
+    {
+        LanguageName : "cpp",
+        LanguageFile : "cp"
+    },
+    {
+        LanguageName : "python",
+        LanguageFile : "py"
+    }
+]
+
+mycoding.forEach( (item) =>{
+
+    console.log(item.LanguageName);
+    
+
 })
