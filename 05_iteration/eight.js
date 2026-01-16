742b737328c4e62a812b2efd776c6141332244b7
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-16 12:14:35 +0000

    reduce

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -1,9 +1,35 @@
 const mynums =[1,2,3,4]
 
-const mytotals = mynums.reduce(function (acc, curval) {
-    console.log(`acc : ${acc} and curval ${curval}`);
+// const mytotals = mynums.reduce(function (acc, curval) {
+//     console.log(`acc : ${acc} and curval ${curval}`);
     
-    return acc + curval
-},  3)
+//     return acc + curval
+// },  3)
 
-console.log(mytotals);
+const mytotals = mynums.reduce( (acc,curval) => acc + curval, 3)
+
+//console.log(mytotals);
+
+const shoppingCart = [
+    {
+        itemName : "Js Cource",
+        price : 600,
+    },
+    {
+        itemName : "Java Cource",
+        price : 1600,
+    },
+    {
+        itemName : "JQuery Cource",
+        price : 900,
+    },
+    {
+        itemName : "C# Cource",
+        price : 1600,
+    }
+]
+
+const pricetoPay = shoppingCart.reduce ( (acc , item) => acc + item.price,  0)
+
+console.log(pricetoPay);
+
